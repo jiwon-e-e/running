8170647a89a1827b3db339d2b5c8faec8239ca12
kwonjiwon <kjw81024@gmail.com>
2026-01-16 11:47:43 +0900

    New: day02 step2 initially commit

---
+++ src/spartaclub/spartaclub/running/day02/Step2/Main.java
@@ -0,0 +1,30 @@
+package spartaclub.spartaclub.running.day02.Step2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //햄버거 셰프 고용
+        HamburgerChef burgerKing = new HamburgerChef();
+
+        // 어떤 손님이 와서 지옥버거 주문
+        BurgerRecipe hellRecipe = new BurgerRecipe() {
+            //클래스 블록이라 field도 만들 수 있음
+            @Override
+            public void cook() {
+                System.out.println("# 지옥에서 만든 보라색 소스 준비");
+            }
+        };
+
+        burgerKing.setRecipe(hellRecipe);
+        burgerKing.makeBurger();
+
+        burgerKing.setRecipe(new BurgerRecipe() {
+            @Override
+            public void cook() {
+                System.out.println("# 양상추 3장 ");
+                System.out.println("# 아보카도 듬뿍~");
+            }
+        });
+        burgerKing.makeBurger();
+    }
+}
